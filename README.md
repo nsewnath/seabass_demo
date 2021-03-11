821d82459119bb883e52eca06b9b3311d1f7c01f
Neeka Sewnath <nsewnath@ufl.edu>
2021-03-11 15:12:17 -0500

    added js and seabass extras feature

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # SeaBASS Data Product Demo
-This is a small demo of using a web application interface to extract data from a SeaBASS file. Other functions could be added to this in the future. 
+This is a small demo of using a web application interface to extract or add data from a SeaBASS file. Other functions could be added to this in the future. 
 
 SB_support.py from https://seabass.gsfc.nasa.gov/wiki/readsb_python
 
@@ -8,7 +8,7 @@ SeaBASS files only.
 Test file from https://seabass.gsfc.nasa.gov/archive/BIGELOW/BALCH/AL9807/archive
 
 # Outputs 
-CSV files for metadata or data table export. Additionally, options for adding the time in XX:XX format as well as the date in DAY/MONTH/YEAR format are now avaliable for the data table routine.
+CSV files for metadata or data table export. Options for adding the time in XX:XX format as well as the date in DAY/MONTH/YEAR format are avaliable for the data table routine. Additionally, now users can select the Add Extras to SeaBASS feature, which will allow the user to add concatenated time or date directly to the original SeaBASS file. 
 
 If no routine is selected then the original SeaBASS file will be returned unmodified. 
 
--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+"""
+Web Application for Data Extraction of a SeaBASS File using Flask. 
+
+Author: Neeka Sewnath, nsewnath@ufl.edu
+
+"""
+
 from flask import Flask
 from flask_cors import CORS
 from flask import request
@@ -5,7 +12,10 @@ from flask import render_template, redirect, send_file
 import pandas as pd
 import numpy as np
 import os
+from collections import OrderedDict
 from SB_support import *
+
+#==========================================================================================================================================
 
 app = Flask(__name__)
 CORS(app)
@@ -20,8 +30,10 @@ def routine():
 
   # Gather routine request
   routine = request.form.get('routine')
-  time = request.form.get('time')
-  date = request.form.get('date')
+  time1 = request.form.get('time1')
+  date1 = request.form.get('date1')
+  time2 = request.form.get('time2')
+  date2 = request.form.get('date2')
 
   # Gather file
   file = request.files['file']
@@ -33,7 +45,7 @@ def routine():
   data = readSB(filename = path, no_warn = True)
 
   if routine == 'metadata':
-    # Create new dataframe and assign ordered list keys and values as columns
+  # Create new dataframe and assign ordered list keys and values as columns
     new_df = pd.DataFrame()
     new_df = new_df.assign(Labels = data.headers.keys())
     new_df = new_df.assign(Data = data.headers.values())
@@ -46,18 +58,18 @@ def routine():
                   as_attachment=True)
 
   elif routine == 'table':
-    # Create new dataframe from SeaBASS data table
+  # Create new dataframe from SeaBASS data table
     new_df = pd.DataFrame(data.data)
 
-    if time:
-      # Creates a column that concatenates the hour and minute columns to XX:XX format
+    if time1:
+    # Creates a column that concatenates the hour and minute columns to XX:XX format
       new_df = new_df.assign(mod_minute = new_df["minute"].apply(str))
       new_df['mod_minute'] = new_df['mod_minute'].apply(lambda x: x.zfill(2))
       new_df = new_df.assign(complete_time = new_df["hour"].apply(str) + ":" + new_df["mod_minute"])
       new_df = new_df.drop(columns = "mod_minute")
 
-    if date:
-      # Creates a column that concatenates the day, month, and year to international standard DAY/MONTH/YEAR
+    if date1:
+    # Creates a column that concatenates the day, month, and year to international standard DAY/MONTH/YEAR
       new_df = new_df.assign(complete_date = new_df["day"].apply(str) + "/" + new_df["month"].apply(str) + "/" + new_df["year"].apply(str))
 
     # Outputting file to user
@@ -67,13 +79,52 @@ def routine():
                   attachment_filename='data_table.csv',
                   as_attachment=True)
 
-  else:
-  # Return the original SeaBASS File
+  elif routine == 'modsb':
+  # Outputs SeaBASS file with add-ons
+
+    new_df = pd.DataFrame(data.data)
+
+    if time2:
+    # Creates a column that concatenates the hour and minute columns to XX:XX format
+      new_df = new_df.assign(mod_minute = new_df["minute"].apply(str))
+      new_df['mod_minute'] = new_df['mod_minute'].apply(lambda x: x.zfill(2))
+      new_df = new_df.assign(complete_time = new_df["hour"].apply(str) + ":" + new_df["mod_minute"])
+      new_df = new_df.drop(columns = "mod_minute")
+
+      fields = data.headers['fields']
+      units = data.headers['units']
+
+      new_field = fields + "," + "completed_time"
+      new_unit = units + "," + "hh:mm"
+
+      data.headers['fields']  = new_field
+      data.headers['units'] = new_unit
+
+    if date2:
+    # Creates a column that concatenates the day, month, and year to international standard DAY/MONTH/YEAR
+      new_df = new_df.assign(complete_date = new_df["day"].apply(str) + "/" + new_df["month"].apply(str) + "/" + new_df["year"].apply(str))
+
+      fields = data.headers['fields']
+      units = data.headers['units']
+
+      new_field = fields + "," + "completed_year"
+      new_unit = units + "," + "dd/mm/yyyy"
+
+      data.headers['fields']  = new_field
+      data.headers['units'] = new_unit
+
+    # Return the original SeaBASS File
+    new_data = OrderedDict(new_df)
+    data.data = new_data
+
+    # Return the original SeaBASS File
     redirect('http://localhost:5000')
-    data.writeSBfile("./unmodified_data.sb")
-    return send_file('./unmodified_data.sb',
-                    attachment_filename='unmodified_data.sb',
+    data.writeSBfile("./seabass_w_extras.sb")
+    return send_file('./seabass_w_extras.sb',
+                    attachment_filename='seabass_w_extras.sb',
                     as_attachment=True)
+
+#===========================================================================================================================================
 
 if __name__ == '__main__':
     app.run(debug=True)
