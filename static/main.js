5ff94e5a1536e80ab20699fbd4268087deaa1b23
Neeka Sewnath <nsewnath@ufl.edu>
2021-03-12 20:12:20 -0500

    fixed bugs

--- static/main.js
+++ static/main.js
@@ -1,19 +1,20 @@
 $(document).ready(function(){
+  $(".form").hide();
+  $(".form1").hide();
+
+  $(".form-check-input2").click(function(){
+    $(".form").show();
+    $(".form1").hide();
+  })
+
+  $(".form-check-input3").click(function(){
+    $(".form1").show();
+    $(".form").hide();
+  })
+
+  $(".form-check-input1").click(function(){
     $(".form").hide();
     $(".form1").hide();
-
-    $(".form-check2").click(function(){
-      $(".form").show();
-      $(".form1").hide();
-    })
-
-    $(".form-check3").click(function(){
-      $(".form1").show();
-      $(".form").hide();
-    })
-
-    $(".form-check1").click(function(){
-      $(".form").hide();
-      $(".form1").hide();
-    })
+  })
 }); 
+
